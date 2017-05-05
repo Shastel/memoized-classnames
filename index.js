2a494a5fd6887eefe905af8d808a91d63cb1273b
Max Shastel <max.shastik@yandex.ru>
2017-05-05 15:12:09 +0300

    Fix lint issues, add keywords

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import cx from 'classnames';
 
 function defaultMemoize(fn) {
-  let cache = {};
-  function memoized(...args) {
-      const hash = JSON.stringify(args);
+    let cache = {};
+    function memoized(...args) {
+        const hash = JSON.stringify(args);
 
-      if (cache[hash]) {
+        if (cache[hash]) {
+            return cache[hash];
+        }
+
+        cache[hash] = fn(...args);
         return cache[hash];
-      }
+    }
 
-      cache[hash] = fn(...args);
-      return cache[hash];
-  }
-
-  memoized.cache = {};
-  memoized.dropCache = function() {
-    cache = {};
     memoized.cache = {};
-  }
+    memoized.dropCache = function () {
+        cache = {};
+        memoized.cache = {};
+    };
 
-  return memoized;
+    return memoized;
 }
 
 export function classNamesCreator(memoize = defaultMemoize) {
